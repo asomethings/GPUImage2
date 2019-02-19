17e2935196bfed7d9837b09e7d4deadfe8f526e8
asomethings <asomethings@hexlant.com>
2019-02-19 11:51:52 +0900

    Added Custom LookupFilters

---
+++ framework/LookupFilters.swift
@@ -0,0 +1,247 @@
+#if !os(Linux)
+
+public class OriginalFilter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_ORIGINAL.png")})()
+        ({intensity = 1.0})()
+    }
+}
+
+public class Summer01Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_SUMMER_01.png")})()
+        ({intensity = 1.0})()
+    }
+}
+
+public class Summer02Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_SUMMER_02.png")})()
+        ({intensity = 1.0})()
+    }
+}
+
+public class Summer03Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_SUMMER_03.png")})()
+        ({intensity = 1.0})()
+    }
+}
+
+public class Summer04Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_SUMMER_04.png")})()
+        ({intensity = 1.0})()
+    }
+}
+
+public class Autumn01Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_AUTUMN_01.png")})()
+        ({intensity = 1.0})()
+    }
+}
+
+public class Autumn02Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_AUTUMN_02.png")})()
+        ({intensity = 1.0})()
+    }
+}
+
+public class Autumn03Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_AUTUMN_03.png")})()
+        ({intensity = 1.0})()
+    }
+}
+
+public class Autumn04Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_AUTUMN_04.png")})()
+        ({intensity = 1.0})()
+    }
+}
+
+public class Some01Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_SOME_01.png")})()
+        ({intensity = 1.0})()
+    }
+}
+
+public class Some02Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_SOME_02.png")})()
+        ({intensity = 1.0})()
+    }
+}
+
+public class Some03Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_SOME_03.png")})()
+        ({intensity = 1.0})()
+    }
+}
+public class Some04Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_SOME_04.png")})()
+        ({intensity = 1.0})()
+    }
+}
+//    public class Some05Filter: LookupFilter {
+//        public override init() {
+//            super.init()
+//
+//            ({lookupImage = PictureInput(imageName:"lookup_SOME_05.png")})()
+//            ({intensity = 1.0})()
+//        }
+//    }
+public class One01Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_ONE_01.png")})()
+        ({intensity = 1.0})()
+    }
+}
+public class One02Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_ONE_02.png")})()
+        ({intensity = 1.0})()
+    }
+}
+public class One03Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_ONE_03.png")})()
+        ({intensity = 1.0})()
+    }
+}
+public class One04Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_ONE_04.png")})()
+        ({intensity = 1.0})()
+    }
+}
+//    public class One05Filter: LookupFilter {
+//        public override init() {
+//            super.init()
+//
+//            ({lookupImage = PictureInput(imageName:"lookup_ONE_05.png")})()
+//            ({intensity = 1.0})()
+//        }
+//    }
+public class Very01Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_VERY_01.png")})()
+        ({intensity = 1.0})()
+    }
+}
+public class Very02Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_VERY_02.png")})()
+        ({intensity = 1.0})()
+    }
+}
+public class Very03Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_VERY_03.png")})()
+        ({intensity = 1.0})()
+    }
+}
+public class Very04Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_VERY_04.png")})()
+        ({intensity = 1.0})()
+    }
+}
+//    public class Very05Filter: LookupFilter {
+//        public override init() {
+//            super.init()
+//
+//            ({lookupImage = PictureInput(imageName:"lookup_VERY_05.png")})()
+//            ({intensity = 1.0})()
+//        }
+//    }
+public class Special01Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_SPECIAL_01.png")})()
+        ({intensity = 1.0})()
+    }
+}
+public class Special02Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_SPECIAL_02.png")})()
+        ({intensity = 1.0})()
+    }
+}
+public class Special03Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_SPECIAL_03.png")})()
+        ({intensity = 1.0})()
+    }
+}
+public class Special04Filter: LookupFilter {
+    public override init() {
+        super.init()
+        
+        ({lookupImage = PictureInput(imageName:"lookup_SPECIAL_04.png")})()
+        ({intensity = 1.0})()
+    }
+}
+//    public class Special05Filter: LookupFilter {
+//        public override init() {
+//            super.init()
+//
+//            ({lookupImage = PictureInput(imageName:"lookup_SPECIAL_05.png")})()
+//            ({intensity = 1.0})()
+//        }
+//    }
+#endif
+
